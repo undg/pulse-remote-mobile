84e6ba2fa87478752ea3d6cad186c3b2113f1b6c
undg <und3rdg@gmail.com>
2026-01-19 01:30:17 +0000

    Add themed tab icons for navigation bar

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 import { StatusBar } from 'expo-status-bar'
 import { ThemeProvider, useTheme } from 'theme'
 import { SinksScreen } from 'screens/Sinks'
@@ -10,12 +11,37 @@ import { AboutScreen } from 'screens/About'
 const Tab = createBottomTabNavigator()
 
 function AppInner() {
-  const { navigationTheme } = useTheme()
+  const { navigationTheme, colors } = useTheme()
 
   return (
     <NavigationContainer theme={navigationTheme}>
       <StatusBar style='auto' />
-      <Tab.Navigator initialRouteName='Sinks'>
+      <Tab.Navigator
+        initialRouteName='Sinks'
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName: keyof typeof Ionicons.glyphMap
+
+            switch (route.name) {
+              case 'Sinks':
+                iconName = 'volume-high-outline'
+                break
+              case 'Sources':
+                iconName = 'mic-outline'
+                break
+              case 'Config':
+                iconName = 'settings-outline'
+                break
+              default:
+                iconName = 'information-circle-outline'
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />
+          },
+          tabBarActiveTintColor: colors.primary,
+          tabBarInactiveTintColor: colors.muted,
+        })}
+      >
         <Tab.Screen name='Sinks' component={SinksScreen} />
         <Tab.Screen name='Sources' component={SourcesScreen} />
         <Tab.Screen name='Config' component={ConfigScreen} />
